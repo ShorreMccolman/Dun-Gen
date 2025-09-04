853c61ab0191189c43ef65caea1fde91bbb17788
Shorre McColman <shorremccolman@gmail.com>
2025-09-04 02:06:59 -0600

    Custom Tile Object and Inspector
    Created a premade tile class for custom rooms
    Created a basic custom inspector for telling generator what layout it uses
    Created a simple example premade room

---
+++ Assets/Scripts/Editor/PremadeTileInspector.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PremadeTile))]
+public class PremadeTileInspector : Editor
+{
+    PremadeTile _tile;
+    int _width;
+    int _height;
+
+    private void OnEnable()
+    {
+        _tile = (PremadeTile)target;
+        _width = _tile.Width;
+        _height = _tile.Height;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        _tile.Width = EditorGUILayout.IntField("Width: ", _tile.Width);
+        _tile.Height = EditorGUILayout.IntField("Height: ", _tile.Height);
+
+        if (_tile.Sprites == null || _tile.Sprites.Length != _width * _height)
+        {
+            _tile.Sprites = new Sprite[_width * _height];
+        }
+
+        for (int i = 0; i < _width; i++)
+        {
+            GUILayout.BeginHorizontal();
+            for (int j = 0; j < _height; j++)
+            {
+                int index = i + j * _width;
+                _tile.Sprites[index] = (Sprite)EditorGUILayout.ObjectField(_tile.Sprites[index], typeof(Sprite), true, GUILayout.Width(100f), GUILayout.Height(100f));
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/Scripts/PremadeTile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PremadeTile : MonoBehaviour
+{
+    [SerializeField] public int Width = 1;
+    [SerializeField] public int Height = 1;
+
+    [SerializeField] public Sprite[] Sprites;
+}
